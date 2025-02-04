48fecbb6bb3e10e987b6dc2a7ed26eb7dbda4dce
ViTu0x70 <vitor.navarro4@hotmail.com>
2025-02-04 15:52:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+string frase = Console.ReadLine();
+string x = Console.ReadLine();
+string y = Console.ReadLine();
+string z = Console.ReadLine();
+
+string s = Console.ReadLine();
+string[] vetor = s.Split(' '); //split deixa um espaço entrer o que for incerido no vetor
+string a = vetor[0];
+string b = vetor[1];  
+string c = vetor[2];
+
+Console.WriteLine("você digitou: " + frase + ", " + x + ", "+ y +", "+ z);
+Console.WriteLine("você digitou: " + a + ", " + b + ", " + c);
